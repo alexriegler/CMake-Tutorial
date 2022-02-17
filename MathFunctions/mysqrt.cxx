0202efce525bb2334f79db7ec0b5788a68530ed7
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2022-02-17 01:36:20 -0600

    Add Step 5: Adding System Introspection

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,14 +1,21 @@
-#include <iostream>
-
 #include "MathFunctions.h"
+
+#include <cmath>
+
+#include <iostream>
 
 // a hack square root calculation using simple operations
 double mysqrt(double x)
 {
-  if (x <= 0) {
-    return 0;
-  }
+    if (x <= 0) {
+        return 0;
+    }
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+              << " using log and exp" << std::endl;
+#else
   double result = x;
 
   // do ten iterations
@@ -20,5 +27,7 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
+#endif
+
   return result;
 }
